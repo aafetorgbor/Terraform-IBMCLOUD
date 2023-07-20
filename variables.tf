5e609e30455ee1d35ced62d2c4761a5966b0c5dc
aafetorgbor <85805640+aafetorgbor@users.noreply.github.com>
2023-07-20 12:46:44 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,35 +1,34 @@
 variable "kibana_password" {
   description = "Kibana password"
   type        = string
-  default = "${kibana_password}"
+  default = ""
   sensitive   = true
 }
 
 variable "elastic_password" {
   description = "Elastic password"
   type        = string
-  default = "${elastic_password}"
+  default = ""
   sensitive   = true
 }
 
 variable "database_password" {
   description = "Database password-new"
   type        = string
-  default = "${database_password}"
+  default = ""
   sensitive   = true
 }
 
 variable "access_key" {
   description = "Remote state file access key"
   type        = string
-  default = "${access_key}"
+  default = ""
   sensitive   = true
 }
 
 variable "secret_key_key" {
   description = "Remote state file secret key"
   type        = string
-  default = "${secret_key}"
+  default = ""
   sensitive   = true
 }
-
